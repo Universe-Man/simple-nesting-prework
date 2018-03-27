a930cf915694a7203e0748264ac0309d586d16e8
Ian Pollack <ianjpollack@gmail.com>
2018-03-27 05:07:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,12 +59,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
-
 	programmer_hash =
  		{ 	:yukihiro_matsumoto => {
 		    :known_for => "Ruby",
@@ -83,8 +77,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
 end
 
 def changing_alan
